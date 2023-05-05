2dbcd11343dc5a018da2ad16411742fe60dd57f6
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2023-05-05 19:28:14 +0200

    fix: installation script

---
+++ install-betterphp.sh
@@ -0,0 +1,13 @@
+RELEASE_URL=https://github.com/ManuelPuchner/BetterPHP/releases/latest
+
+# Get the latest release information
+release=$(curl -s $RELEASE_URL)
+
+# Extract the download URL for the latest release asset
+download_url=$(echo "$release" | jq -r '.assets[].browser_download_url' | grep -E '\.tar\.gz$' | head -1)
+
+# Download the asset to the current directory
+curl -L "$download_url" -o "$(basename "$download_url")"
+
+# Extract the downloaded tarball (optional)
+tar -xvf "$(basename "$download_url")"
